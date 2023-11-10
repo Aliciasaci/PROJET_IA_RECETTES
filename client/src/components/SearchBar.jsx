90fdf0bdc24d8616c3c88ec7c3c3d991d41364ba
Alicia Saci <lyciiasci@gmail.com>
2023-11-10 15:05:43 +0100

    display recipe generation

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
+import axios from 'axios';
+
 export default function searchBar() {
+
+    const [searchPrompt, setSearchPrompt] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleInputChange = (e) => {
+        setSearchPrompt(e.target.value);
+    }
+
+    const handleSearch = () => {
+        axios.post('http://localhost:5000/generate', { searchPrompt })
+            .then(response => {
+                const jsonObject = JSON.parse(response.data.assistantResponse);
+                setSearchResult(jsonObject);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+
     return (
         <div className="searchbar-wrapper">
-            <input class="input is-rounded searchbar" type="text" placeholder="Saisissez votre recherche..."></input>
-            <button class="button is-rounded">Soumettre</button>
+            <input className="input is-rounded searchbar" type="text" placeholder="Saisissez votre recherche..."
+            value={searchPrompt} onChange={handleInputChange}></input>
+            <button class="button is-rounded" onClick={handleSearch}>Soumettre</button>
         </div>
     )
 }
