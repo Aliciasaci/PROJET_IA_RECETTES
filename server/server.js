8f72c659c537664bf574030cb5f50e0d8e709814
Alicia Saci <lyciiasci@gmail.com>
2023-12-22 15:38:37 +0100

    fetch similar recipes + detail of recipe

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.post("/fetchTitles", async (req, res) => {
     const completions = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: messages,
-      format : "json",
+      format: "json",
     });
 
     const assistantResponse = completions.choices[0].message.content;
@@ -59,7 +59,6 @@ async function fetchRecettesByTitle(recettes) {
     const recettesArray = JSON.parse(recettes);
     const client = await pool.connect();
     const promises = recettesArray.map((recette) => {
-      console.log("recette" + recette);
       return client.query("SELECT * FROM recettes WHERE titre = $1", [recette]);
     });
 
@@ -82,6 +81,68 @@ app.post("/fetchRecettesByTitle", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+
+async function fetchRecetteById(recetteId) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM recettes WHERE id = $1", [recetteId]);
+    const data = result.rows[0];
+    client.release();
+    return data;
+  } catch (error) {
+    console.error("Error executing query", error);
+    throw error;
+  }
+}
+
+app.get("/fetchRecetteById/:id", async (req, res) => {
+  const recetteId = req.params.id;
+  try {
+    const recetteData = await fetchRecetteById(recetteId);
+    if (recetteData) {
+      res.json({ recetteData });
+    } else {
+      res.status(404).json({ message: "Recette non trouvée" });
+    }
+  } catch (error) {
+    console.error("Error processing request", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+async function fetchSimilarRecipes(recetteTitle) {
+  const recettes = await fetchRecettes();
+  try {
+    const completions = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: `En te basant sur ces recettes ${JSON.stringify(recettes)}. recommandes toutes celles qui ressemblent à la recette suivante : ${JSON.stringify(recetteTitle)}. renvoi sous format json et uniquement les titres` },
+      ],
+      format: "json",
+    });
+
+    result = completions.choices[0].message.content;
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error("Error executing query", error);
+    throw error;
+  }
+}
+
+app.get("/fetchSimilarRecipes", async (req, res) => {
+  const titre = req.query.titre;
+  try {
+    const similarRecipes = await fetchSimilarRecipes(titre);
+    res.json({ similarRecipes });
+  } catch (error) {
+    console.error("Error processing request", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
