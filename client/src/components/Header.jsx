69bd8ae1c40c688991dbe8306f6f92b76d75d3e3
Awa Bah <bah.awaa@gmail.com>
2024-01-12 02:42:18 +0100

    link to home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { AppBar, Button, Container, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import useAuth from '../hooks/useAuth';
+import React from "react";
+import {
+  AppBar,
+  Button,
+  Container,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function Header() {
   const { auth, setAuth } = useAuth();
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="#">
+        <a className="navbar-item" href="/">
           <div className="logo">CuisineConnect</div>
         </a>
       </div>
@@ -18,36 +26,67 @@ export default function Header() {
           <Link className="navbar-item" to={"/"}>
             Accueil
           </Link>
-         
         </div>
 
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
-              {
-                auth?.email
-                  ? (
-                    <>
-                      <Button className="signin-btn mr-2" component={Link} to={"/me"} style={{ color: 'black', border : '1px solid orangered', padding: '0.5rem 1rem' }}>
-                        Bienvenue { auth.nom } { auth.prenom}
-                      </Button>
-                      <Button className="signup-btn" component={Link} to={"/"} style={{ color: 'white', backgroundColor: 'orangered', padding: '0.5rem 1rem' }} onClick={() => setAuth({})}>
-                        Se déconnecter
-                      </Button>
-                    </>
-                  )
-                  : (
-                  <>
-                    <Button className="signin-btn mr-2" component={Link} to={"/signin"} style={{ color: 'black', border : '1px solid orangered', padding: '0.5rem 1rem' }}>
+              {auth?.email ? (
+                <>
+                  <Button
+                    className="signin-btn mr-2"
+                    component={Link}
+                    to={"/me"}
+                    style={{
+                      color: "black",
+                      border: "1px solid orangered",
+                      padding: "0.5rem 1rem",
+                    }}
+                  >
+                    Bienvenue {auth.nom} {auth.prenom}
+                  </Button>
+                  <Button
+                    className="signup-btn"
+                    component={Link}
+                    to={"/"}
+                    style={{
+                      color: "white",
+                      backgroundColor: "orangered",
+                      padding: "0.5rem 1rem",
+                    }}
+                    onClick={() => setAuth({})}
+                  >
+                    Se déconnecter
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    className="signin-btn mr-2"
+                    component={Link}
+                    to={"/signin"}
+                    style={{
+                      color: "black",
+                      border: "1px solid orangered",
+                      padding: "0.5rem 1rem",
+                    }}
+                  >
                     Se connecter
-                    </Button>
-                    <Button className="signup-btn" component={Link} to={"/signup"} style={{ color: 'white', backgroundColor: 'orangered', padding: '0.5rem 1rem' }}>
-                      S'inscrire
-                    </Button>
-                  </>
-                  )
-              }
-              
+                  </Button>
+                  <Button
+                    className="signup-btn"
+                    component={Link}
+                    to={"/signup"}
+                    style={{
+                      color: "white",
+                      backgroundColor: "orangered",
+                      padding: "0.5rem 1rem",
+                    }}
+                  >
+                    S'inscrire
+                  </Button>
+                </>
+              )}
             </div>
           </div>
         </div>
